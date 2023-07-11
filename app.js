a6346fc0bec4b952a5a68d2cff87d3d458fc77a0
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-07-11 14:54:48 +0700

    Finish in layout & Templating

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -22,11 +23,26 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", function (req, res) {
-  item = req.body.newItem;
+  let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.button === "Work") {
+    workItems.push(item);
 
-  res.redirect("/");
+    res.redirect("/work");
+  }else {
+    items.push(item);
+  
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { kindOfDay: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, () => {
